33779b3d0aff8c4e857f348927c8d46d992119bc
Quang <20378quang@gmail.com>
2024-11-02 13:08:23 -0400

    initial

---
+++ flask-server/server.py
@@ -0,0 +1,18 @@
+from flask import Flask
+import jinja2
+import os
+
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape=True)
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    template = jinja_env.get_template('index.html')
+    return template.render()
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
